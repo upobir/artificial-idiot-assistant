9fe0df1e21c087a3cb557000128f44bb5f6cb132
Md Sabbir Rahman <33892003+upobir@users.noreply.github.com>
2024-10-12 02:19:36 +0600

    add env values

--- cmd/aia/main.go
+++ cmd/aia/main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
+type logWriter struct{}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Printf("[%s] %s\n", time.Now().Format("2006/01/02 15:04:05"), string(bytes))
+}
+
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v\n", err)
+	}
+
 	reader := bufio.NewReader(os.Stdin)
+
+	log.Printf("Secret env value is %s\n", os.Getenv("TEST"))
 
 	for {
 		fmt.Print("you > ")
