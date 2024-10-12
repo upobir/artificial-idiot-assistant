fb09e9473638e219541317baa2976ac0bc03fc02
Md Sabbir Rahman <33892003+upobir@users.noreply.github.com>
2024-10-12 14:12:58 +0600

    add mongo

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Artificial Idiot Assistant (AIA)
-## Running
+## Development
+### Mongo DB
+Use a mongo db for local testing, suggested to use docker. Use following yaml file
+```yaml
+version: '3.7'
+services:
+  db:
+    image: mongo:latest
+    environment:
+      MONGO_INITDB_ROOT_USERNAME: root
+      MONGO_INITDB_ROOT_PASSWORD: rootpassword
+    ports:
+      - 27017:27017
+    volumes:
+      - mongodb_data_container:/data/db
+volumes:
+  mongodb_data_container:
+```
+and run `docker compose up`
+
+
+### Run
 Run with 
 ```bash
 go run ./cmd/aia
--- cmd/aia/main.go
+++ cmd/aia/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/joho/godotenv"
 	"github.com/upobir/artificial-idiot-assistant/internal/console"
@@ -9,11 +16,51 @@ import (
 )
 
 func main() {
-	utils.InitializeLog()
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v\n", err)
+	}
+
+	utils.InitializeLog()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:rootpassword@localhost:27017"))
+
+	if err != nil {
+		log.Fatalf("Client connect failed: %v", err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatalf("Client disconnect failed: %v", err)
+		}
+	}()
+	{
+		var result bson.M
+		if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+			panic(err)
+		}
+
+		log.Printf("DB connected")
+	}
+
+	{
+		db := client.Database("aia-dev")
+		collection := db.Collection("test")
+
+		testDocument := map[string]any{
+			"name":      "test",
+			"value":     123,
+			"timestamp": time.Now(),
+		}
+
+		result, err := collection.InsertOne(ctx, testDocument)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Inserted document ID:", result.InsertedID)
 	}
 
 	console.Run()
